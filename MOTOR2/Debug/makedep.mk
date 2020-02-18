76f74320f1c4bf1e7d88364e019ba87a346d38de
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-18 13:57:09 +0200

    wo

---
+++ MOTOR2/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+DIO.c
+
+DIO_config.c
+
+Motor.c
+
+main.c
+
+Motor_Config.c
+
+PWM.c
+
+PWM_Config.c
+
--- MOTOR2/Motor.c
+++ MOTOR2/Motor.c
@@ -3,5 +3,32 @@
  *
  * Created: 18/02/2020 01:38:30 م
  *  Author: mo
- */ 
+ */
 #include "motor.h"
+
+
+
+ERROR_STATUS Motor_Init(uint8_t Motor_Number)
+{uint8_t Ret=0;
+
+
+    switch(Motor_Number)
+    {
+        case MOTOR_1:
+        /*Enable PWM Bit...output*/
+        /*IN1*/
+        /*IN2*/
+			Ret=Pwm_Init(&PWM_Configuration_1A);
+			Ret=DIO_init(&Motorone);
+
+			  break;
+
+          case MOTOR_2:
+		      Ret=Pwm_Init(&PWM_Configuration_1B);
+          Ret=DIO_init(&Motortwo);
+          break;
+    }
+
+
+return Ret;
+}
