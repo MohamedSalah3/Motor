a001e0b69dd837fb125770c01f734eff3178d3d7
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-18 13:40:19 +0200

    Re doing Motor

---
+++ MOTOR2/Motor.c
@@ -0,0 +1,7 @@
+﻿/*
+ * Motor.c
+ *
+ * Created: 18/02/2020 01:38:30 م
+ *  Author: mo
+ */ 
+#include "motor.h"
---
+++ MOTOR2/main.c
@@ -0,0 +1,17 @@
+﻿/*
+ * MOTOR2.c
+ *
+ * Created: 18/02/2020 01:35:31 م
+ *  Author: mo
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
