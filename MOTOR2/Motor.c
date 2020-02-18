ef6456a4f91d3f80d197d0090bad65d5fc4e10f2
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-18 14:01:35 +0200

    d

--- MOTOR2/Motor.c
+++ MOTOR2/Motor.c
@@ -6,8 +6,6 @@
  */
 #include "motor.h"
 
-
-
 ERROR_STATUS Motor_Init(uint8_t Motor_Number)
 {uint8_t Ret=0;
 
@@ -31,4 +29,108 @@ ERROR_STATUS Motor_Init(uint8_t Motor_Number)
 
 
 return Ret;
+}
+
+ERROR_STATUS Motor_Direction(uint8_t Motor_Number, uint8_t Motor_Direction)
+{
+
+  switch(Motor_Number)
+  {
+  	case MOT_1:{
+  	switch(Motor_Direction)
+  	{
+  		case MOTOR_FORWARD:
+  		{
+  			DIO_Write(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,HIGH);
+  			DIO_Write (MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,LOW);
+
+  			break;
+  		}
+  		case MOTOR_BACKWARD:
+  		{
+  			DIO_Write(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,LOW);
+  			DIO_Write(MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,HIGH);
+
+  			break;
+  		}
+  		case MOTOR_STOP:{DIO_Write(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,LOW);
+  		DIO_Write(MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,LOW);
+  		break;}
+
+  	}
+  		break;
+  	}
+  	case MOTOR_2:
+  	{
+  		switch(Motor_Direction)
+  		{
+  			case MOTOR_FORWARD:
+  			{
+  				DIO_Write(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,LOW);
+  				DIO_Write(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,HIGH);
+
+  				break;
+  			}
+  			case MOTOR_BACKWARD:
+  			{
+  				DIO_Write(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,HIGH);
+  				DIO_Write(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,LOW);
+  				break;
+  			}
+  			case MOTOR_STOP:
+  			{
+  				DIO_Write(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,LOW);
+  				DIO_Write(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,LOW);
+  				break;
+  			}
+
+  		}
+
+  		break;
+  	}
+  	case ALLMOT:
+  	{
+  switch(en_motor_dir)
+  {
+  	case FORWARD:
+  	{
+  		gpioPinWrite(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,HIGH);
+  		gpioPinWrite(MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,LOW);
+
+  		gpioPinWrite(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,LOW);
+  		gpioPinWrite(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,HIGH);
+
+  		break;
+  	}
+  	case BACKWARD:
+  	{
+
+  		gpioPinWrite(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,LOW);
+  		gpioPinWrite(MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,HIGH);
+
+  		gpioPinWrite(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,HIGH);
+  		gpioPinWrite(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,LOW);
+
+  		break;	}
+  	case STOP:
+  	{
+  			gpioPinWrite(MOTOR_OUT_1A_GPIO,MOTOR_OUT_1A_BIT,LOW);
+  			gpioPinWrite(MOTOR_OUT_2A_GPIO,MOTOR_OUT_2A_BIT,LOW);
+
+  			gpioPinWrite(MOTOR_OUT_1B_GPIO,MOTOR_OUT_1B_BIT,LOW);
+  			gpioPinWrite(MOTOR_OUT_2B_GPIO,MOTOR_OUT_2B_BIT,LOW);
+
+  		break;
+  	}
+  	}
+
+  }
+  	break;
+  	}
+
+
+
+
+
+
 }
